db08106c24a2f3126dcd743f2ab6064aacd89404
muhammedorhungunes <gunesmuhammedorhun@gmail.com>
2025-04-22 11:46:04 +0300

    add to bad-single-responsibility-principle

---
+++ bad-single-responsibility-principle/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BadSingleResponsibilityPrinciple;
+
+Console.WriteLine("Hello, World!");
+        var account = new Account
+        {
+            AccountId = Guid.NewGuid(),
+            FirstName = "Orhun",
+            LastName = "gunes",
+            Street = "123 Main St",
+            ZipCode = 38190,
+            City = "Kayseri",
+            Password = "123",
+            Username = "name"
+            //Country = "Türkiye" 
+        };
+
+        account.ChangePassword("321");
+        account.DepositMoney(100);
